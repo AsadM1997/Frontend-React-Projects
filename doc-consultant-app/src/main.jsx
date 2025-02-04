ab5ee113df47355d683bce613592a5727a3c2654
AsadM1997 <asadm1059@gmail.com>
2025-02-04 15:40:04 +0530

    AppContextCompleted

--- doc-consultant-app/src/main.jsx
+++ doc-consultant-app/src/main.jsx
@@ -7,8 +7,7 @@ import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <AppContextProvider> <App /></AppContextProvider>
-   
+    <AppContextProvider> <App /></AppContextProvider>
+
   </BrowserRouter>,
 )
- 
