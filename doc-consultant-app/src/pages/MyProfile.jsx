110a1bbddbc0a64cf6235c5688ba5504091d2fd4
AsadM1997 <asadm1059@gmail.com>
2025-03-03 22:02:06 +0530

    working on myprofile page

--- doc-consultant-app/src/pages/MyProfile.jsx
+++ doc-consultant-app/src/pages/MyProfile.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
-const MyProfile = () => {
+const MyProfile = (user) => {
+
+  const [userData, setUserData] = useState({
+    name: "Edwerd vincent",
+    image: assets.profile_pic,
+    email: 'richardjameswap@gmail.com',
+    phone: '+1 000 000 0000',
+    address: {
+      line1: "57th cross , Richmonds",
+      line2: "circle,church Road, London"
+    },
+    gender: "Male",
+    dob: '2000-01-20'
+  })
+
+  const [isEdit, setIsEdit] = useState(true)
+
   return (
-    <div>MyProfile</div>
-  )
+    <div className='max-w-lg flex flex-col gap-2 text-sm ' >
+      <img className=" w-36 rounded" src={userData.image} alt="" />
+
+      {isEdit ? (
+        <input 
+        className='bg-gray-50  text-3xl font-medium max-w-60 mt-4 '
+          type="text"
+          value={userData.name}
+          onChange={(e) =>
+            setUserData((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      ) : (
+        <p className='font-medium text-3xl  text-neutral-800 mt-4'>{userData.name}</p>
+      )}
+
+      <hr className='bg-zinc-400 h-[1px] border-none' />
+      <div>
+        <p className='text-neutral-500 mt-3 underline'>CONTACT INFORMATION</p>
+        <div>
+          <p>Email Id:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+          {isEdit ? (
+            <input
+              className=" bg-gray-100 max-w-52"
+              type="text"
+              value={userData.phone}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, phone: e.target.value }))
+              }
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+          <p>address:</p>
+          {
+            isEdit ?
+              <p >
+                <input onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} value={userData.address.line1} type='text' />
+                <br />
+                <input onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} value={userData.address.line2} type='text' />
+              </p>
+              : <p>
+                {userData.address.line1}
+                <br />
+                {userData.address.line2}
+              </p>
+
+          }
+
+        </div>
+      </div>
+
+      <div>
+        <p>BASIC INFORMATION</p>
+        <div>
+          <p>Gender:</p>
+          {isEdit
+            ? <select onChange={(e) => setUserData(prev => ({ ...prev => ({ ...prev, gender: e.target.value }) }))} value={userData.gender}>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+            : <p>{userData.gender}</p>
+          }
+          <p>Birthday: </p>
+          {
+            isEdit
+              ? <input type='date' onChange={(e) => setUserData(prev => ({ ...prev => ({ ...prev, dob: e.target.value }) }))} value={userData.dob} />
+              : <p>{userData.dob}</p>
+          }
+        </div>
+      </div>
+
+      <div>
+        {
+          isEdit
+            ? <button onClick={() => setIsEdit(false)}>Save Information</button>
+            : <button onClick={() => setIsEdit(true)}>Edit</button>
+        }
+      </div>
+
+    </div>
+  );
 }
 
 export default MyProfile
